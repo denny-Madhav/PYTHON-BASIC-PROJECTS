8c073f34a937f52cc00956feb1cf0924ffaacbbd
denny-Madhav <56473453+denny-Madhav@users.noreply.github.com>
2021-05-18 12:40:05 +0530

    Band name generator

--- hmm.py
+++ hmm.py
@@ -2,5 +2,4 @@ print(" BAND NAME GENERATOR  \nPlease enter your State  or Country name \n")
 CorS = input()
 print('Enter your pet name ')
 pet = input()
-
 print("Your band name could be "+CorS+"n"+"\t"+pet+"..!")
