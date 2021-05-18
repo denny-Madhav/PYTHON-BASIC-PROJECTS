f6e7bc337bd28c1f38b435f22082cea1538e859d
denny-Madhav <56473453+denny-Madhav@users.noreply.github.com>
2021-05-18 12:37:46 +0530

    weeks in life

---
+++ life_in_weeks.py
@@ -0,0 +1,15 @@
+age = input("your age ");
+dage=90;
+
+day=365;
+week = 52;
+month = 12;
+
+rage=dage-int(age);
+
+rdays = rage * day;
+rweeks = rage * week;
+rmonth = rage * month;
+
+print(f"you have {rdays} days, {rweeks} weeks and {rmonth} months left ..! ")
+
