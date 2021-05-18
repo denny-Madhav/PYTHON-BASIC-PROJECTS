3c10db8714a89c83cf712b37383086fc2ee0e14d
denny-Madhav <56473453+denny-Madhav@users.noreply.github.com>
2021-05-18 09:52:14 +0530

    Add files via upload

---
+++ hmm.py
@@ -0,0 +1,6 @@
+print(" BAND NAME GENERATOR  \nPlease enter your State  or Country name \n")
+CorS = input()
+print('Enter your pet name ')
+pet = input()
+
+print("Your band name could be "+CorS+"n"+"\t"+pet+"..!")
