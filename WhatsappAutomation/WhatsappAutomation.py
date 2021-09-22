055e4aa4cd76667a128ec55b95ae324318cd797c
Denny Madhav <56473453+denny-Madhav@users.noreply.github.com>
2021-09-22 20:22:26 +0530

    pycharm

---
+++ WhatsappAutomation/WhatsappAutomation.py
@@ -0,0 +1,4 @@
+import pywhatkit as kit
+
+kit.sendwhatmsg("+919108371209", "This message sent using whatsapp automation by denny :-) ", time_hour=22, time_min=30)
+kit.sendwhatmsg("+918405966111", "This message sent using whatsapp automation by denny :-) ", time_hour=22, time_min=30)
